c63dc85eea583d55be472f8e15bae34ec1e13a53
Jason Justice <jayjustice@gmail.com>
2025-12-02 16:30:06 -0800

    lots of updates

--- start_all_docker.sh
+++ start_all_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker compose -f ./docker_services/arr/docker-compose.yml up -d --remove-orphans
-docker compose -f ./docker_services/seedbox/docker-compose.yml up -d --remove-orphans
-# docker compose -f ./docker_services/plex/docker-compose.yml up -d
+docker compose -f ./docker_services/download_agents/docker-compose.yml up -d --remove-orphans
+docker compose -f ./docker_services/plex/docker-compose.yml up -d
--- stop_all_docker.sh
+++ stop_all_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker compose -f ./docker_services/arr/docker-compose.yml down
-docker compose -f ./docker_services/seedbox/docker-compose.yml down
-# docker compose -f ./docker_services/plex/docker-compose.yml down
+docker compose -f ./docker_services/download_agents/docker-compose.yml down
+docker compose -f ./docker_services/plex/docker-compose.yml down
