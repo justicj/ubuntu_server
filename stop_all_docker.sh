6a08f2190d9463b81d7df3559329332e96d9385b
Jason Justice <jayjustice@gmail.com>
2025-12-03 16:41:45 -0800

    adjust script

--- stop_all_docker.sh
+++ stop_all_docker.sh
@@ -2,4 +2,5 @@
 
 docker compose -f ./docker_services/arr/docker-compose.yml down
 docker compose -f ./docker_services/download_agents/docker-compose.yml down
+docker compose -f ./docker_services/infrastructure/docker-compose.yml down
 docker compose -f ./docker_services/plex/docker-compose.yml down
