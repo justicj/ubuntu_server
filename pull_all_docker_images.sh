2e31fc65e7ca4707405827a111dc9caac20f798f
Jason Justice <jayjustice@gmail.com>
2025-12-03 19:18:28 -0800

    forgot one

--- pull_all_docker_images.sh
+++ pull_all_docker_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker compose -f ./docker_services/arr/docker-compose.yml pull
-docker compose -f ./docker_services/download_agents/docker-compose.yml pull
+docker compose -f ./docker_services/download_agents/docker-compose.yml pull sabnzbd
 docker compose -f ./docker_services/infrastructure/docker-compose.yml pull
 docker compose -f ./docker_services/plex/docker-compose.yml pull
