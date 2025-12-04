77faf79642661ef80767f0f5fa00e4f37e33f9df
Jason Justice <jayjustice@gmail.com>
2025-12-03 19:19:35 -0800

    user is plex

--- backup_appdata.sh
+++ backup_appdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./stop_all_docker.sh
 
-tar -zcvf /home/jason/backups/appdata_$(date +%F).tar.gz /appdata
+tar -zcvf /home/plex/backups/appdata_$(date +%F).tar.gz /appdata
 
 ./start_all_docker.sh
